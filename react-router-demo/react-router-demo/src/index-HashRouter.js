2572a5397694e42d49c1c1849db0c5a727b21068
wb-sbw342082 <wb-sbw342082@antfin.com>
2018-07-05 19:19:04 +0800

    router study 1

---
+++ react-router-demo/react-router-demo/src/index-HashRouter.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {HashRouter, Route, Link} from 'react-router-dom';
+
+const About = () => (
+    <div>
+        <h3>关于我们</h3>
+    </div>
+);
+const Home = () => (
+    <div>
+        <h3>关于首页</h3>
+    </div>
+);
+
+const Header = () => (
+    <div>
+        <h3> React-Router 路由</h3>
+        <ul>
+            <li><Link exact to="/"></Link>首页</li>
+            <li><Link to="/home">主页</Link></li>
+            <li><Link to="/about">关于</Link></li>
+        </ul>
+    </div>
+)
+
+const Main = () => (
+    <div>
+        <Route path="/home" component={Home}/>
+        <Route path="/about" component={About}/>
+    </div>
+);
+
+class App extends React.Component {
+
+
+    render() {
+        return (<div>
+                <Header/>
+                <Main/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <HashRouter>
+        <App/>
+    </HashRouter>
+    , document.querySelector("#root"));
--- react-router-demo/react-router-demo/src/index.js
+++ react-router-demo/react-router-demo/src/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link,Switch} from 'react-router-dom';
+import uuid from "uuid";
+const stuData = {
+    stuList:[
+        {id:uuid(),name:"张三",sex:"男",tel:"1234566778"},
+        {id:uuid(),name:"李四",sex:"男",tel:"1334566778"},
+        {id:uuid(),name:"王五",sex:"女",tel:"1434566778"},
+        {id:uuid(),name:"赵六",sex:"男",tel:"1534566778"},
+        {id:uuid(),name:"牛七",sex:"女",tel:"1634566778"},
+
+    ]
+}
+
 
 const About = () => (
-
     <div>
         <h3>关于我们</h3>
     </div>
 );
 const Home = () => (
     <div>
-        <h3>关于首页</h3>
+        <h3>关于SBWIT</h3>
+    </div>
+);
+
+const Stu = () => (
+    <div>
+        <h3>学生信息</h3>
     </div>
 );
 
@@ -18,22 +35,24 @@ const Header = () => (
     <div>
         <h3> React-Router 路由</h3>
         <ul>
-            <li><Link to="#/home">主页</Link></li>
-            <li><Link to="#/about">关于</Link></li>
+            <li><Link  to="/">主页</Link></li>
+            <li><Link to="/about">关于</Link></li>
+            <li><Link to="/stu">学生</Link></li>
         </ul>
     </div>
 )
 
 const Main = () => (
     <div>
-        <Route path="/home" component={Home}/>
-        <Route path="/about" component={About}/>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/stu" component={Stu}/>
+        </Switch>
     </div>
 );
 
 class App extends React.Component {
-
-
     render() {
         return (<div>
                 <Header/>
@@ -44,7 +63,7 @@ class App extends React.Component {
 }
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
         <App/>
-    </HashRouter>
+    </BrowserRouter>
     , document.querySelector("#root"));
