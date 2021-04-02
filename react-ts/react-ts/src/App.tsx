28d47eb2d07f8b444386c64db78ca1261b39c863
sbwit <17740857130@163.com>
2021-04-02 22:32:26 +0800

    Component

--- react-ts/react-ts/src/App.tsx
+++ react-ts/react-ts/src/App.tsx
@@ -1,10 +1,14 @@
 import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppTest from './AppTest'
 
 class App extends Component<any,any>{
     render() {
-      return (<div>test react ts</div>)
+      return (<div>
+        <h1>App 首页</h1>
+        <AppTest/>
+      </div>)
     }
 }
 
---
+++ react-ts/react-ts/src/AppTest.tsx
@@ -0,0 +1,11 @@
+import React,{ Component } from "react";
+
+export default class AppTest extends Component<any,any> {
+    render() {
+        return (
+            <div>
+                <h1>app 11111111</h1>
+            </div>
+        );
+    }
+}
